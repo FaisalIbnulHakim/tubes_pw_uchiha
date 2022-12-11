b5b4170c43913b69056992839d0f134e286584fb
203040019ikhsan <ikhsanardiansyah0018@gmail.com>
2022-12-12 00:54:58 +0700

    about

---
+++ resources/views/about.blade.php
@@ -0,0 +1,11 @@
+<section class="page-section bg-orange" id="about">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-8 text-center">
+            <h1 class="text-black mt-0">About Us</h1>
+            <hr class="divider light my-4" />
+            <p class="text-black-50 mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor a maiores, ab exercitationem beatae nulla consequuntur deleniti tenetur non dolorum id est velit doloremque animi, unde optio fugiat sint earum?</p>
+        </div>
+    </div>
+</div>
+</section>
