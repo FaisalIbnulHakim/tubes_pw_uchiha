c665d15dbc605bbc2dfd33cdcf21deb1c4c19477
203040019ikhsan <ikhsanardiansyah0018@gmail.com>
2023-01-03 06:38:55 +0700

    memperbaiki halaman about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -7,9 +7,9 @@
         <div class="p-4 p-md-5 mt-4 mb-4 text-white rounded bg-dark">
         <div class="row justify-content-center">
             <div class="col-8 text-center">
-            <h1>About Us</h1>
+            <h1 class="text-white">About Us</h1>
             <hr class="divider light my-4">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor a maiores, ab exercitationem beatae nulla consequuntur deleniti tenetur non dolorum id est velit doloremque animi, unde optio fugiat sint earum?</p>
+            <p>UchihaTech adalah aplikasi yang menyediakan daftar review handphone,laptop, dan kamera untuk membantu anda yang sedang mencari rekomendasi produk tersebut.</p>
             </div>
         </div>
         </div>
@@ -24,28 +24,70 @@
     </div>
     
     <div class="row bg-dark text-center">
-    <div class="text-white col-lg-40">
-        <img class="rounded-circle " src="https://img.freepik.com/free-photo/digital-device-eletronic-networking-media_53876-31695.jpg?w=900&amp;t=st=1670398025~exp=1670398625~hmac=17ec9b6fd63cdb55365b58c64c7878a4d4fd416279549b352b2c4bc118d1b41c" width="180" height="180">
-        <h3>Ikhsan</h3>
-        <p>Ikhsan Ardiansyah | 203040019 | About</p>
+     <div class="d-flex mx-auto p-4" style="width: 600px">
+        <img class="rounded-circle" src="https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/323423418_523918026382951_4832149994164770252_n.jpg?stp=dst-jpg_s600x600&_nc_cat=105&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeHDIrS5q6WjTQxrPcOtKGFjlr-Cci40tfyWv4JyLjS1_MdS8i24GCnO3t1U4aq0CWe24fdBBhf635GkfKJIxnCb&_nc_ohc=CtnSbndyyDIAX-DKg8l&_nc_ht=scontent-sin6-2.xx&oh=00_AfCQjJm1aaNRjIIMb1iT0C6uxJNVY6F8sArhSTsz424NtA&oe=63B8D26E" width="180" height="180">
+        <div class="text-white text-center">
+        <br>
+        <h3 class="p-0 mr-auto" style="width: 600px">Ikhsan Ardiansyah</h3>
+        <p class="p-0">Frontend</p>
+        <a class="btn btn-warning" href="#">
+        <i class="bi bi-instagram"></i></a>
+        <a class="btn btn-dark" href="#">
+        <i class="bi bi-github"></i></a>
+        <a class="btn btn-primary" href="#">
+        <i class="bi bi-linkedin"></i></a>
+        <a class="btn btn-danger" href="#">
+        <i class="bi bi-envelope"></i></a>
+        <a class=""></a>
+    </div>
+    </div>
+    <div class="d-flex mx-auto p-4" style="width: 600px" >
+            <img class="rounded-circle" src="https://scontent-sin6-4.xx.fbcdn.net/v/t39.30808-6/323441717_958936385079567_8533704633971691014_n.jpg?stp=dst-jpg_p526x296&_nc_cat=101&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeF4F-xZ-BzuhpKmDH2BKg6VIc5vUrf28iAhzm9St_byIOCl4wXvB6o9tZUzGJTQQs_KSNU92hlp2V4gB4cy16jn&_nc_ohc=cwVrg2d7byQAX_Aq2nP&_nc_ht=scontent-sin6-4.xx&oh=00_AfDtUJrqycbuzOS_5IUa67jvYAoyr_d3i9fqITxBKrMdFg&oe=63B7DBBD" width="180" height="180">
+            <div class="text-white text-center">
+            <br>
+            <h3 class="p-0 mr-auto" style="width: 600px">Mustofa Firdaus</h3>
+            <p class="p-0">Backend</p>
+            <a class="btn btn-warning" href="#">
+            <i class="bi bi-instagram"></i></a>
+            <a class="btn btn-dark" href="#">
+            <i class="bi bi-github"></i></a>
+            <a class="btn btn-primary" href="#">
+            <i class="bi bi-linkedin"></i></a>
+            <a class="btn btn-danger" href="#">
+            <i class="bi bi-envelope"></i></a>
         </div>
-    <div class="row jutify-content-center">
-        <div class="text-white col-lg-40">
-            <img class="rounded-circle" src="https://img.freepik.com/free-photo/digital-device-eletronic-networking-media_53876-31695.jpg?w=900&amp;t=st=1670398025~exp=1670398625~hmac=17ec9b6fd63cdb55365b58c64c7878a4d4fd416279549b352b2c4bc118d1b41c" width="180" height="180">
-            <h3>Ikhsan</h3>
-            <p>Ikhsan Ardiansyah | 203040019 | About</p>
         </div>
-    <div class="row jutify-content-center">
-            <div class="text-white col-lg-40">
-                <img class="rounded-circle" src="https://img.freepik.com/free-photo/digital-device-eletronic-networking-media_53876-31695.jpg?w=900&amp;t=st=1670398025~exp=1670398625~hmac=17ec9b6fd63cdb55365b58c64c7878a4d4fd416279549b352b2c4bc118d1b41c" width="180" height="180">
-                <h3>Ikhsan</h3>
-                <p>Ikhsan Ardiansyah | 203040019 | About</p>
+    <div class="d-flex mx-auto p-4" style="width: 600px">
+        <img class="rounded-circle" src="https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/323340237_636944664879127_3677424539311954808_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeFtR4s4k4IqxCG2waG5KsaYlxkZ2Cr6-kmXGRnYKvr6SbnmoyYxms_-wh61q7F7MzH9O7jtAB_w_spSwYz6vfiu&_nc_ohc=1DjsYI2-N6YAX92krcD&tn=72B4V2t1FQ_9f_Tj&_nc_ht=scontent-sin6-2.xx&oh=00_AfCdatzz50n878V671xZV6vHpW9G0N4ldCxcYldHdfLA3Q&oe=63B83B44" width="180" height="180">
+        <div class="text-white text-center">
+        <br>
+        <h3 class="p-0 mr-auto" style="width: 600px">Faisal Ibnul Hakim</h3>
+        <p class="p-0">Backend</p>
+        <a class="btn btn-warning" href="#">
+        <i class="bi bi-instagram"></i></a>
+        <a class="btn btn-dark" href="#">
+        <i class="bi bi-github"></i></a>
+        <a class="btn btn-primary" href="#">
+        <i class="bi bi-linkedin"></i></a>
+        <a class="btn btn-danger" href="#">
+        <i class="bi bi-envelope"></i></a>
         </div>
-    <div class="row jutify-content-center">
-            <div class="text-white col-lg-40">
-                <img class="rounded-circle" src="https://img.freepik.com/free-photo/digital-device-eletronic-networking-media_53876-31695.jpg?w=900&amp;t=st=1670398025~exp=1670398625~hmac=17ec9b6fd63cdb55365b58c64c7878a4d4fd416279549b352b2c4bc118d1b41c" width="180" height="180">
-                <h3>Ikhsan</h3>
-                <p>Ikhsan Ardiansyah | 203040019 | About</p>
         </div>
+    <div class="d-flex mx-auto p-4" style="width: 600px">
+        <img class="rounded-circle" src="https://i.pinimg.com/236x/a3/00/29/a30029ee5ccb77d89446fb38791deecc.jpg" width="180" height="180">
+        <div class="text-white text-center">
+        <br>
+        <h3 class="p-0 mr-auto" style="width: 600px">Zaki Auliya Azhari</h3>
+        <p class="p-0">Frontend</p>
+        <a class="btn btn-warning" href="#">
+        <i class="bi bi-instagram"></i></a>
+        <a class="btn btn-dark" href="#">
+        <i class="bi bi-github"></i></a>
+        <a class="btn btn-primary" href="#">
+        <i class="bi bi-linkedin"></i></a>
+        <a class="btn btn-danger" href="#">
+        <i class="bi bi-envelope"></i></a>
+        </div>
+ </div>
  </div>
 @endsection
