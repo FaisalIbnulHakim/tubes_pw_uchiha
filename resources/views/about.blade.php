b2bf4b25fa897cd45629bfa799d2e595eb566737
203040019ikhsan <ikhsanardiansyah0018@gmail.com>
2023-01-03 19:00:46 +0700

    menyelesaikan halaman about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,12 +4,14 @@
 @section('container')
 <div class="page-section bg-white" id="about">
     <div class="container">
-        <div class="p-4 p-md-5 mt-4 mb-4 text-white rounded bg-dark">
+        <div class="p-4 p-md-5 mt-7 mb-4 text-white rounded bg-dark about-page">
         <div class="row justify-content-center">
             <div class="col-8 text-center">
             <h1 class="text-white">About Us</h1>
             <hr class="divider light my-4">
-            <p>UchihaTech adalah aplikasi yang menyediakan daftar review handphone,laptop, dan kamera untuk membantu anda yang sedang mencari rekomendasi produk tersebut.</p>
+            <p>UchihaTech adalah aplikasi yang menyediakan review-review smartphone, laptop dan kamera untuk membantu anda yang sedang mencari rekomendasi produk tersebut. Kami memberikan review 
+                yang jujur agar anda terbantu dengan review kami serta mendapat insight baru tentang gadget yang ada saat ini.
+            </p>
             </div>
         </div>
         </div>
@@ -19,75 +21,81 @@
 
     <div class="row bg-dark">
     <div class="container-fluid heading">
-        <h1 class=" text-white text-center pb-4">Team</h1>
+        <h1 class=" text-white text-center pb-4 mt-4">Team Uchiha</h1>
     </div>
     </div>
     
     <div class="row bg-dark text-center">
-     <div class="d-flex mx-auto p-4" style="width: 600px">
+     <div class="d-lg-flex mx-auto p-4" style="width: 600px">
         <img class="rounded-circle" src="https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/323423418_523918026382951_4832149994164770252_n.jpg?stp=dst-jpg_s600x600&_nc_cat=105&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeHDIrS5q6WjTQxrPcOtKGFjlr-Cci40tfyWv4JyLjS1_MdS8i24GCnO3t1U4aq0CWe24fdBBhf635GkfKJIxnCb&_nc_ohc=CtnSbndyyDIAX-DKg8l&_nc_ht=scontent-sin6-2.xx&oh=00_AfCQjJm1aaNRjIIMb1iT0C6uxJNVY6F8sArhSTsz424NtA&oe=63B8D26E" width="180" height="180">
         <div class="text-white text-center">
         <br>
-        <h3 class="p-0 mr-auto" style="width: 600px">Ikhsan Ardiansyah</h3>
-        <p class="p-0">Frontend</p>
-        <a class="btn btn-warning" href="#">
+        <h3 class="p-0 mr-auto nama-dev">Ikhsan Ardiansyah</h3>
+        <p class="p-0">Frontend Developer</p>
+        <a class="btn btn-warning" target="_blank" href="https://www.instagram.com/ikhsanardiansyah1803/">
         <i class="bi bi-instagram"></i></a>
-        <a class="btn btn-dark" href="#">
+        <a class="btn btn-dark" target="_blank" href="https://github.com/203040019Ikhsan">
         <i class="bi bi-github"></i></a>
-        <a class="btn btn-primary" href="#">
+        <a class="btn btn-primary" target="_blank" href="http://linkedin.com/in/ikhsan-ardiansyah-2a1509260/">
         <i class="bi bi-linkedin"></i></a>
-        <a class="btn btn-danger" href="#">
+        <a class="btn btn-danger" target="_blank" href="mailto:Ikhsanardiansyah0018@gmail.com">
         <i class="bi bi-envelope"></i></a>
         <a class=""></a>
     </div>
     </div>
-    <div class="d-flex mx-auto p-4" style="width: 600px" >
+    <div class="d-lg-flex mx-auto p-4" style="width: 600px" >
             <img class="rounded-circle" src="https://scontent-sin6-4.xx.fbcdn.net/v/t39.30808-6/323441717_958936385079567_8533704633971691014_n.jpg?stp=dst-jpg_p526x296&_nc_cat=101&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeF4F-xZ-BzuhpKmDH2BKg6VIc5vUrf28iAhzm9St_byIOCl4wXvB6o9tZUzGJTQQs_KSNU92hlp2V4gB4cy16jn&_nc_ohc=cwVrg2d7byQAX_Aq2nP&_nc_ht=scontent-sin6-4.xx&oh=00_AfDtUJrqycbuzOS_5IUa67jvYAoyr_d3i9fqITxBKrMdFg&oe=63B7DBBD" width="180" height="180">
             <div class="text-white text-center">
             <br>
-            <h3 class="p-0 mr-auto" style="width: 600px">Mustofa Firdaus</h3>
-            <p class="p-0">Backend</p>
-            <a class="btn btn-warning" href="#">
-            <i class="bi bi-instagram"></i></a>
-            <a class="btn btn-dark" href="#">
+            <h3 class="p-0 mr-auto nama-dev">Mustofa Firdaus</h3>
+            <p class="p-0">Backend Developer</p>
+            <a class="btn btn-warning" target="_blank" href="https://www.instagram.com/mustfrds_/">
+            <i class="bi bi-instagram" ></i></a>
+            <a class="btn btn-dark" target="_blank" href="https://github.com/mustofa855">
             <i class="bi bi-github"></i></a>
-            <a class="btn btn-primary" href="#">
+            <a class="btn btn-primary" target="_blank" href="https://www.linkedin.com/in/mustofa-firdaus-460195217">
             <i class="bi bi-linkedin"></i></a>
-            <a class="btn btn-danger" href="#">
+            <a class="btn btn-danger" target="_blank" href="mailto:mustofafirdaus354@gmail.com">
             <i class="bi bi-envelope"></i></a>
         </div>
         </div>
-    <div class="d-flex mx-auto p-4" style="width: 600px">
+    <div class="d-lg-flex mx-auto p-4" style="width: 600px">
         <img class="rounded-circle" src="https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/323340237_636944664879127_3677424539311954808_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeFtR4s4k4IqxCG2waG5KsaYlxkZ2Cr6-kmXGRnYKvr6SbnmoyYxms_-wh61q7F7MzH9O7jtAB_w_spSwYz6vfiu&_nc_ohc=1DjsYI2-N6YAX92krcD&tn=72B4V2t1FQ_9f_Tj&_nc_ht=scontent-sin6-2.xx&oh=00_AfCdatzz50n878V671xZV6vHpW9G0N4ldCxcYldHdfLA3Q&oe=63B83B44" width="180" height="180">
         <div class="text-white text-center">
         <br>
-        <h3 class="p-0 mr-auto" style="width: 600px">Faisal Ibnul Hakim</h3>
-        <p class="p-0">Backend</p>
-        <a class="btn btn-warning" href="#">
+        <h3 class="p-0 mr-auto nama-dev">Faisal Ibnul Hakim</h3>
+        <p class="p-0">Project Manager</p>
+        <a class="btn btn-warning" target="_blank" href="https://www.instagram.com/fiuls_hakim/">
         <i class="bi bi-instagram"></i></a>
-        <a class="btn btn-dark" href="#">
+        <a class="btn btn-dark" target="_blank" href="https://github.com/FaisalIbnulHakim">
         <i class="bi bi-github"></i></a>
-        <a class="btn btn-primary" href="#">
+        <a class="btn btn-primary" target="_blank" href="https://www.linkedin.com/in/faisal-hakim-444684260/">
         <i class="bi bi-linkedin"></i></a>
-        <a class="btn btn-danger" href="#">
+        <a class="btn btn-danger" target="_blank" href="mailto:kamhakim2@gmail.com">
         <i class="bi bi-envelope"></i></a>
         </div>
         </div>
-    <div class="d-flex mx-auto p-4" style="width: 600px">
-        <img class="rounded-circle" src="https://i.pinimg.com/236x/a3/00/29/a30029ee5ccb77d89446fb38791deecc.jpg" width="180" height="180">
+    <div class="d-lg-flex mx-auto p-4" style="width: 600px">
+        <img class="rounded-circle" src="https://scontent-sin6-1.xx.fbcdn.net/v/t39.30808-6/322583466_3583254418480128_5574907237002729314_n.jpg?stp=dst-jpg_p526x296&_nc_cat=111&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeHYCO66ZdTneB9OmEmH1Ff2loX-gc8w9xeWhf6BzzD3F8TqybyUhNZppP_t1lox2Uc4lWMBqG373_Yxznj8Sv8C&_nc_ohc=nwEpEoXgOBUAX9biPUu&_nc_ht=scontent-sin6-1.xx&oh=00_AfAiizew5z89lCIX8dp_apMt2mnxjOlRyTNq346ZUvjDnQ&oe=63B85525" width="180" height="180">
         <div class="text-white text-center">
         <br>
-        <h3 class="p-0 mr-auto" style="width: 600px">Zaki Auliya Azhari</h3>
+        <h3 class="p-0 mr-auto nama-dev">Zaki Auliya Azhari</h3>
         <p class="p-0">Frontend</p>
-        <a class="btn btn-warning" href="#">
+        <a class="btn btn-warning" target="_blank" href="https://www.instagram.com/zakiaazhari14/">
         <i class="bi bi-instagram"></i></a>
-        <a class="btn btn-dark" href="#">
+        <a class="btn btn-dark" target="_blank" href="https://github.com/zakiaazhari14">
         <i class="bi bi-github"></i></a>
-        <a class="btn btn-primary" href="#">
+        <a class="btn btn-primary" target="_blank" href="https://www.linkedin.com/in/zaki-auliya-azhari-250514260/">
         <i class="bi bi-linkedin"></i></a>
-        <a class="btn btn-danger" href="#">
+        <a class="btn btn-danger" target="_blank" href="mailto:zakiauliyaazhari@gmail.com">
         <i class="bi bi-envelope"></i></a>
         </div>
  </div>
  </div>
+
+ <footer>
+    <div class="text-center p-3 text-white" style="background-color: rgb(22, 22, 22);">
+        © 2022 Uchiha. All rights reserved.
+      </div>
+ </footer>
 @endsection
